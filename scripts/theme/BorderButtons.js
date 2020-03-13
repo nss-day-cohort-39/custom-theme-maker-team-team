95f2c468d576e50f86bd663127a7526094f565a1
Mark McCann <mjmccann12@me.com>
2020-03-13 14:22:37 -0500

    completed news border

--- scripts/theme/BorderButtons.js
+++ scripts/theme/BorderButtons.js
@@ -1,18 +1,33 @@
+const eventHub = document.querySelector("#container")
+eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.name === "pixelsize") {
+        const pixelChosen = clickEvent.target.value
+        const pixelChosenEvent = new CustomEvent("pixelSizeChosen", {
+            detail: {
+                pixelSize: pixelChosen
+            }
+        })
+        eventHub.dispatchEvent(pixelChosenEvent)
+    }
+})
+
+
+
+
+
+
 export const BorderButtons = () => {
     return `
-        <article class="borderSizes">
-            <fieldset>
-                <legend>Border Sizes</legend>
-
-                <label for="onepixel">1px</label>
-                <input type="radio" name="pixelsize" value="onepixel" />
-
-                <label for="threepixels">3px</label>
-                <input type="radio" name="pixelsize" value="threepixels" />
-
-                <label for="fivepixels">5px</label>
-                <input type="radio" name="pixelsize" value="fivepixels" />
-            </fieldset>
-        </article>
+    <article class="borderSizes">
+    <fieldset>
+        <legend>Border Sizes</legend>
+        <label for="onepixel">1px</label>
+        <input type="radio" name="pixelsize" value="onepixel" />
+        <label for="threepixels">3px</label>
+        <input type="radio" name="pixelsize" value="threepixels" />
+        <label for="fivepixels">5px</label>
+        <input type="radio" name="pixelsize" value="fivepixels" />
+    </fieldset>
+</article>
     `
 }
