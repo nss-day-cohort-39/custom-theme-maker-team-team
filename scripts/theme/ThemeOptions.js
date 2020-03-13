ebb44902bb13076227d4a119dd589162391fe1f3
blake slochowsky <blakeslochowsky.com>
2020-03-13 09:07:04 -0500

    background color functional

--- scripts/theme/ThemeOptions.js
+++ scripts/theme/ThemeOptions.js
@@ -2,6 +2,17 @@ import { ColorButtons } from "./ColorButtons.js"
 import { FontButtons } from "./FontButtons.js"
 import { BorderButtons } from "./BorderButtons.js"
 
+const eventHub = document.querySelector("#container")
+
+eventHub.addEventListener("backgroundColorChange", theEvent=>{
+    const colorSelected = theEvent.detail.colorChange
+
+    eventHub.classList = []
+
+    eventHub.classList.add(colorSelected)
+})
+
+
 export const ThemeOptions = () => {
     return `
         <article class="container__panel themeOptions">
